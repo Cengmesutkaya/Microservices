f7f90aedecfe8db61663e6ce53e7c0128784c31d
MESUT KAYA <mesutkaya2000@gmail.com>
2022-01-16 17:24:38 +0300

    [16.01.2022]

--- ContactAPI/Contact.API.Infrastructure/IContactService.cs
+++ ContactAPI/Contact.API.Infrastructure/IContactService.cs
@@ -1,9 +1,4 @@
 ﻿using Contact.API.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Contact.API.Infrastructure
 {
--- ContactAPI/Contact.API.Models/ContactDTO.cs
+++ ContactAPI/Contact.API.Models/ContactDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Contact.API.Models
+﻿namespace Contact.API.Models
 {
     public class ContactDTO
     {
