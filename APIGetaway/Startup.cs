d95930b6434fb9ce9b44c74001db40a1827e1d28
MESUT KAYA <mesutkaya2000@gmail.com>
2022-01-17 11:35:16 +0300

    ...

--- APIGetaway/Startup.cs
+++ APIGetaway/Startup.cs
@@ -14,17 +14,12 @@ namespace APIGateway
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOcelot();
             services.AddControllers();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
